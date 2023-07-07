efe1bd9f0227455360410c7889de638a39764ab5
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-07 23:17:00 +0530

    Create Header.jsx

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import "./Header.css";
+import HeaderCartButton from "./HeaderCartButton";
+import { useAuth } from "../../context/AuthContext";
+import { NavLink } from "react-router-dom";
+
+export default function Header() {
+  const { user, logout } = useAuth();
+  return (
+    <header className="header">
+      <h1>ShoppingApp</h1>
+      {user ? <div style={{ display: "flex" }}>
+        <NavLink
+          onClick={() => {
+            logout();
+          }}
+          className={({ isActive, isPending }) =>
+            isPending ? "pending" : isActive ? "active" : ""
+          }
+        >
+          Logout
+        </NavLink>
+        <HeaderCartButton />
+      </div> :
+        <div>
+          <NavLink
+            to="/login"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : ""
+            }
+          >
+            Login
+          </NavLink>
+
+        </div>}
+
+    </header>
+  );
+}
