ccb5083741ac199efbd7d5d5dba1756bba54d962
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-11 13:44:59 +0530

    Update cart.js

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -1,9 +1,10 @@
-const cartController= require('../controllers/cart')
+const cartController = require('../controllers/cart')
+const { authenticateJWT } = require('../middlewares/authenticate')
 
-const express= require('express')
+const express = require('express')
 
-const router= express.Router()
-router.get('/cart', cartController.getCart)
-router.post('/cart', cartController.postCart)
+const router = express.Router()
+router.get('/cart', authenticateJWT, cartController.getCart)
+router.post('/cart', authenticateJWT, cartController.postCart)
 
-module.exports= router
+module.exports = router
