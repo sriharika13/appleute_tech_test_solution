326f9f9e18321d223889472191ab4c4802ec9e5b
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-11 13:39:56 +0530

    Update Cart.jsx

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,24 +1,41 @@
-export default function Cart(){
-    const cartItems=[
-        {id: 'c1', name: 'Paper'},
-        {id: 'c2', name: 'Pepper'},
-        {id: 'c3', name: 'Shoes'}
-    ].map((item)=>{
-        return <li key={item.id}>{item.name}</li>
-    })
-    return(
+import './Cart.css'
+import { useEffect, useState } from "react"
+import { useCart } from "../context/CartItems"
+
+export default function Cart() {
+    const { cart } = useCart();
+    const [total, setTotal] = useState(0)
+    const [cartItems, setCartItems] = useState([])
+
+    useEffect(() => {
+        if (cart && Object.keys(cart).length > 0) {
+            const values = Object.values(cart);
+            setTotal(values.reduce(
+                (prev, curr) => prev + (curr.qty * curr.price), 0
+            ));
+            setCartItems(values);
+        }
+
+    }, [cart])
+    return (
         <>
-        <ul className="cart-items">
-            {cartItems}
-        </ul>
-        <div  className="total">
-            <span>Total Amount </span>
-            <span>34.4</span>
-        </div>
-        <div className="actions">
-            <button className="button--alt">Close</button>
-            <button className="button">Order</button>
-        </div>
+            <div className="cart-items">
+                {cartItems.map((item) =>
+                    <li key={item.id}>
+                        <div>{item.name}</div>
+                        <div>{item.price}</div>
+                    </li>)
+                }
+            </div>
+
+            <div className="total">
+                <span>Total Amount </span>
+                <span>{total}</span>
+            </div>
+            <div className="actions">
+                <button className="button--alt">Close</button>
+                <button className="button">Order</button>
+            </div>
         </>
     )
 }
