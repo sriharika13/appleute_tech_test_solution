330ae0b50db88c0a7f10d23203cc22913dc6a09f
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-11 13:42:31 +0530

    Update products.js

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -1,14 +1,12 @@
-const Product = require('../models/productModel')
+const Product = require('../models/productModel');
 
-exports.getProducts=(req, res)=>{
-    Product.fetchAll((products)=>{
-        res.send(products)
-    })
-    
+exports.getProducts = async (req, res) => {
+    const products= await Product.fetchAll()
+    res.send(products)
 }
 
-exports.postProduct=(req, res)=>{
-    const p1= new Product("Lamp", "beautiful white lamp", 45.55, "Groceries")
+exports.postProduct = (req, res) => {
+    const p1 = new Product("Lamp", "beautiful white lamp", 45.55, "Groceries")
     p1.save()
 }
 
