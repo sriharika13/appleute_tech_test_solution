34a84b2d97c2dd5926563ef76f03357a2d9022ad
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-11 13:34:45 +0530

    HeaderCartButton.jsx

--- client/src/components/Header/HeaderCartButton.jsx
+++ client/src/components/Header/HeaderCartButton.jsx
@@ -1,19 +1,39 @@
 import CartIcon from "../Cart/CartIcon";
 import { useAuth } from "../../context/AuthContext";
 import "./HeaderCartButton.css";
+import { Link } from "react-router-dom";
+import { useCart } from "../../context/CartItems";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function HeaderCartButton() {
   const { user } = useAuth();
+  const { cart } = useCart();
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    if (cart && Object.keys(cart).length > 0) {
+      setTotal(Object.values(cart).reduce(
+        (prev, curr) => prev + curr.qty, 0
+      ))
+    }
+
+  }, [cart])
+
+
+
+
   return (
     <div className="user_cart">
       <h2>{user.name}</h2>
-      <button className="button">
+      <Link to="/cart"><button className="button" >
         <span className="icon">
           <CartIcon />
         </span>
         <span>Your Cart</span>
-        <span className="badge">0</span>
+        <span className="badge">{total}</span>
       </button>
+      </Link>
     </div>
   );
 }
