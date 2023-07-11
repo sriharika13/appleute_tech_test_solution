679f0c25f43330c811c0bbe9f1ab80e12eff643b
Sriharika Mariyala <56622258+sriharika13@users.noreply.github.com>
2023-07-11 13:43:58 +0530

    Update cartModel.js

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,48 +1,72 @@
-const fs= require('fs')
-const path= require('path')
+const fs = require('fs')
+const path = require('path')
+const Product = require('../models/productModel');
+const { readFileProm, writeFileProm } = require('../utils/fsPromise');
 
-const p= path.join(
-    path.dirname(__dirname), 'data', 'cart.json'
+const p = path.join(
+  path.dirname(__dirname), 'data', 'cart.json'
 )
 
-module.exports=class Cart{
-    static addProduct(id, productPrice) {
-        // Fetch the previous cart
-        fs.readFile(p, (err, fileContent) => {
-          let cart = { products: [], totalPrice: 0 };
-          if (!err) {
-            cart = JSON.parse(fileContent);
-          }
-          // Analyze the cart => Find existing product
-          const existingProductIndex = cart.products.findIndex(
-            prod => prod.id === id
-          );
-          const existingProduct = cart.products[existingProductIndex];
-          let updatedProduct;
-          // Add new product/ increase quantity
-          if (existingProduct) {
-            updatedProduct = { ...existingProduct };
-            updatedProduct.quantity = updatedProduct.quantity + 1;
-            cart.products = [...cart.products];
-            cart.products[existingProductIndex] = updatedProduct;
-          } else {
-            updatedProduct = { id: id, quantity: 1 };
-            cart.products = [...cart.products, updatedProduct];
-          }
-          cart.totalPrice = cart.totalPrice + +productPrice;
-          fs.writeFile(p, JSON.stringify(cart), err => {
-            console.log(err);
-          });
-        });
-      }
+module.exports = class Cart {
+  static async addProduct(id, username) {
 
-      static fetchCart(cb){
-        fs.readFile(p, (err, fileContent)=>{
-          const cart= JSON.parse(fileContent)
-          if(err) cb(null)
-          else {
-            cb(cart)
+    try {
+      const filename = `${username}.json`;
+      let cart = {}
+      if (fs.existsSync(filename)) {
+        const fileContent = await readFileProm(filename);
+        cart = JSON.parse(fileContent);
+        if (cart[id]) {
+          cart[id].qty += 1;
+        } else {
+          const products = await Product.fetchAll();
+          const found = products.find((product) => product.id === id);
+          if (found) {
+            cart[id] = {
+              qty: 1,
+              ...found,
+            }
+          } else {
+            throw new Error('Something Wrong')
           }
-        })
+
+        }
+      } else {
+        const products = await Product.fetchAll();
+        console.log("LL: Cart -> addProduct -> products", id)
+        const found = products.find((product) => product.id === id);
+        if (found) {
+          cart[id] = {
+            qty: 1,
+            ...found,
+          }
+        } else {
+          throw new Error('Something Wrong')
+        }
       }
+      await writeFileProm(filename, JSON.stringify(cart));
+      return cart
+    } catch (error) {
+      console.log("LL: Cart -> addProduct -> error", error)
+      throw new Error('Something Wrong')
+    }
+    // Fetch the previous cart
+
+
+
+  }
+
+  static async fetchCart(username) {
+    try {
+      if (fs.existsSync(`${username}.json`)) {
+        const fileContent = await readFileProm(`${username}.json`);
+        const cart = JSON.parse(fileContent)
+        return cart;
+      } else {
+        return {};
+      }
+    } catch (error) {
+      throw new Error('Something Wrong')
+    }
+  }
 }
